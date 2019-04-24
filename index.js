be612a9615f17ba0cfb2638c27d8245b4d1d02ad
Ken Zhou <mailofrealcev@gmail.com>
2019-04-24 18:42:58 -0400

    displays stuff sometimes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function Site(ref) {
     var that = this;
+    this.elements = {
+        topics: $('#topics'),
+        mainContent: $('#main-content'),
+        posts: $('#posts'),
+        chat: $('#chat'),
+        nothingHere: $('#nothing-here')
+    }
     this.ref = ref;
     this.data = {};
     if (!this.ref) {
@@ -7,13 +14,38 @@ function Site(ref) {
     }
     this.ref.once('value').then(function (snap) {
         that.data = snap.val();
+        that.data.render = function () {
+            that.elements.topics.text('');
+            for (var topic in that.data.topics) {
+                var p = document.createElement('p');
+                p.innerText = topic;
+                that.elements.topics.append(p);
+            }
+            that.elements.nothingHere.hide();
+        };
+        that.data.render();
     }).catch(function (err) {
         document.body.innerHTML = 'Sorry, a database error occured. (' + err + ').';
     });
 };
+
 var app = firebase;
 var database = app.database();
 var auth = app.auth();
 var blitzboardRef = database.ref('blitzboard');
-var ref = blitzboardRef.child('2kofficial');
-var site = new Site();
+
+function Blitzboard(id, structure) {
+    var that = this;
+    this.id = id;
+    this.ref = database.ref('blitzboard/' + this.id);
+    this.structure = structure;
+    this.ref.once('value').then(function (snap) {
+        if (snap.val())
+            return;
+        that.ref.set(structure);
+    });
+}
+
+var site = new Site(new Blitzboard('test', {
+    name: 'test.'
+}).ref);
