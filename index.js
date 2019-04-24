4e37eeda1ea81087e571a6699d14025df1fe3794
Ken Zhou <mailofrealcev@gmail.com>
2019-04-24 17:53:11 -0400

    a good start

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-function Site() {
-  
+function Site(ref) {
+    var that = this;
+    this.ref = ref;
+    this.data = {};
+    if (!this.ref) {
+        return;
+    }
+    this.ref.once('value').then(function (snap) {
+        that.data = snap.val();
+    }).catch(function (err) {
+        document.body.innerHTML = 'Sorry, a database error occured. (' + err + ').';
+    });
 };
+var app = firebase;
+var database = app.database();
+var blitzboardRef = database.ref('blitzboard');
+var ref = blitzboardRef.child('2kofficial');
 var site = new Site();
