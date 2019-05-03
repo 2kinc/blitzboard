630882c345baa1cf39d6eb1f5962425a77eef079
Ken Zhou <mailofrealcev@gmail.com>
2019-05-03 16:39:14 -0400

    file input, does not do anything

--- index.js
+++ index.js
@@ -306,3 +306,24 @@ auth.onAuthStateChanged(function (user) {
         auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
     }
 });
+
+var inputs = document.querySelectorAll( '.inputfile' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
+
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( '\\' ).pop();
+
+		if( fileName )
+			label.innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
